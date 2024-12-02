ad4f5e7da9a5f1bfc264e19eba0fb757b792aa27
NAJIYA KP <najiyabinthmajeed@gmail.com>
2024-12-02 05:40:31 -0800

    get method added

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Col, Container, Row } from 'react-bootstrap'
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
@@ -6,7 +6,7 @@ import { useState } from 'react';
 import Modal from 'react-bootstrap/Modal';
 import FloatingLabel from 'react-bootstrap/FloatingLabel';
 import Form from 'react-bootstrap/Form';
-import { addEvent } from '../services/allApi';
+import { addEvent, getEvent } from '../services/allApi';
 
 
 
@@ -14,21 +14,49 @@ import { addEvent } from '../services/allApi';
 function Landing() {
 
     const [eventDetails, setEventDetails] = useState({ eventName: '', eventPlace: '', eventDate: '', eventStartTime: '', eventEndsTime: '' })
+    const [allEvents, setAllEvents] = useState([])
     const [show, setShow] = useState(false);
 
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
 
 
+    useEffect(() => {
+        getAllEvents()
+    })
 
+
+    // get event from server
+    const getAllEvents = async () => {
+        try {
+            const reslt = await getEvent()
+            // console.log('getEvent')
+            // console.log(reslt.data)
+
+
+            if (reslt.status >= 200 && reslt.status < 300) {
+                // console.log('yes')
+                setAllEvents(reslt.data)
+            }
+        } catch (err) {
+            console.log(err)
+        }
+    }
+
+    // when add event details
     const hanldeAddEvent = async () => {
-        const { eventName, eventPlace, eventDate, eventStartTime, eventEndsTime }=eventDetails
+        const { eventName, eventPlace, eventDate, eventStartTime, eventEndsTime } = eventDetails
         if (eventName && eventPlace && eventDate && eventStartTime && eventEndsTime) {
             console.log('success')
             try {
 
+
                 const res = await addEvent(eventDetails)
-                console.log(res.data)
+                handldate(eventDate)
+                // console.log(res.data)
+                handleClose()
+
+
 
             } catch (err) {
                 console.log(err)
@@ -40,6 +68,11 @@ function Landing() {
 
     }
 
+    const handldate = (evDate) => {
+        console.log(evDate)
+        Month
+    }
+
 
     return (
 
@@ -48,48 +81,67 @@ function Landing() {
 
             <Container>
 
-                <Row>
+                <Row >
+
+
                     <Col lg={8} >
                         <div className='d-flex justify-content-between'>
                             <h2>Events</h2>
-                            <h5>Create Event  <Button variant="outline-dark" onClick={handleShow} className='m-2 pt-1'><i class="fa-solid fa-plus"></i></Button> </h5>
+                            <h5>Create Event  <Button variant="outline-dark" onClick={handleShow} className='m-2 pt-1'>
+                                <i class="fa-solid fa-plus"></i>
+                            </Button>
+                            </h5>
 
 
                         </div>
 
-                        <div className='d-flex'>
-                            <Col lg={4}>
-                                <Card className='border rounded-4 bg-secondary m-2 shadow position-absolute' >
+                        <Row className='d-flex'>
 
-                                    <Card.Body>
-                                        <Card.Title>Music Program</Card.Title>
-                                        <Card.Text>
-                                            <h6>music events</h6>
-                                            <p>2:15- 2:50</p>
-                                        </Card.Text>
-                                    </Card.Body>
-
-                                    <div className='calender-box shadow'>
-                                        <p>
-                                            JUN
-                                        </p>
-                                        <p style={{ marginTop: '-20px' }}>12</p>
-
-                                    </div>
-
-                                </Card>
-
-                            </Col>
+                            {
+                                allEvents.length > 0 ?
+                                    allEvents?.map(ev => (
 
 
+                                        <Col lg={4} >
+                                            <Card className='border rounded-4 bg-secondary m-2 shadow position-absolute' style={{ width: '12rem' }} >
+
+                                                <Card.Body>
+                                                    <Card.Title>{ev.eventName}</Card.Title>
+                                                    <Card.Text>
+                                                        <h6>{ev.eventPlace}</h6>
+                                                        <p>{ev.eventStartTime}-{ev.eventEndsTime}</p>
+                                                    </Card.Text>
+                                                </Card.Body>
+
+                                                <div className='calender-box shadow'>
+                                                    <p>
+                                                        JUN
+                                                    </p>
+                                                    <p style={{ marginTop: '-20px' }}>12</p>
+
+                                                </div>
+
+                                            </Card>
+
+                                        </Col>
 
 
-                        </div>
 
+
+
+
+                                    ))
+                                    :
+                                    <p className='text-danger'>text danger</p>
+                            }
+
+
+
+                        </Row>
 
                     </Col>
-                    <Col>
-
+                    <Col lg={4}>
+                        
                     </Col>
                 </Row>
 
@@ -106,13 +158,13 @@ function Landing() {
                     <Modal.Body>
 
                         {/* event name */}
-                            <FloatingLabel
-                                controlId="floatingTextarea"
-                                label="Event Name"
-                                className="mb-3"
-                            >
-                                <Form.Control onChange={(e) => setEventDetails({ ...eventDetails, eventName: e.target.value })} type='text' placeholder="Event Name" />
-                            </FloatingLabel>
+                        <FloatingLabel
+                            controlId="floatingTextarea"
+                            label="Event Name"
+                            className="mb-3"
+                        >
+                            <Form.Control onChange={(e) => setEventDetails({ ...eventDetails, eventName: e.target.value })} type='text' placeholder="Event Name" />
+                        </FloatingLabel>
 
                         {/* event location */}
                         <FloatingLabel
--- src/services/allApi.js
+++ src/services/allApi.js
@@ -5,3 +5,7 @@ import SERVER_URL from "./server_url";
 export const addEvent = async (evnt) => {
     return await commonAPI("POST", `${SERVER_URL}/allEvents` ,evnt)
 }
+
+export const getEvent =async ()=>{
+    return await commonAPI("GET", `${SERVER_URL}/allEvents`, "")
+}
