5b4b07b78777ccf43188672ba8662b5c43b08fc4
NAJIYA KP <najiyabinthmajeed@gmail.com>
2024-12-10 02:37:25 -0800

    landing completed

--- src/pages/FirstPage.jsx
+++ src/pages/FirstPage.jsx
@@ -10,49 +10,67 @@ function FirstPage() {
         height: '100vh', width: '213vh', backgroundColor: ' #b3cccc'
       }}>
 
+        {/* <div className="header-container" style={{ textAlign: 'center', marginBottom: '1rem' }}>
+  <h1 className="head-style w-100" style={{ margin: 0 }}>Simple Event Management System</h1>
+</div> */}
 
         <div className='circleFour d-flex flex-column justify-content-center shadow'>
-        <div className='circleinnerIn d-flex flex-column justify-content-center shadow text-dark'>
-        <i class="fa-brands fa-readme fa-4x"></i>
-        </div>
+          <div className='circleinnerIn d-flex flex-column justify-content-center shadow text-dark'>
+            <i class="fa-brands fa-readme fa-4x"></i>
+          </div>
         </div>
 
         <div className='circleFive d-flex flex-column justify-content-center shadow '>
-        <div className='circleinnerIn d-flex flex-column justify-content-center shadow text-dark'>
-        <i class="fa-solid fa-pen-to-square fa-4x"></i>
-        </div>
+          <div className='circleinnerIn d-flex flex-column justify-content-center shadow text-dark'>
+            <i class="fa-solid fa-pen-to-square fa-4x"></i>
+          </div>
         </div>
         {/* <div className='circleSeven'></div> */}
 
         <div className='circleNine'></div>
 
         {/* main circle center */}
-        <div className='circleOne d-flex justify-content-center p-2 '>
+        <div data-aos="zoom-in-up" className='circleOne d-flex justify-content-center p-2 ' style={{
+          transform: 'scale(1)', // Initial state
+          transition: 'transform 0.3s ease-in-out', // Smooth transition
+        }} onMouseEnter={(e) => {
+          e.currentTarget.style.transform = 'scale(1.1)'; // Scale up on hover
+        }}
+          onMouseLeave={(e) => {
+            e.currentTarget.style.transform = 'scale(1)'; // Reset on hover out
+          }}>
           {/* <h2 style={{color:'brown'}}> Event Management</h2> */}
           <div className='circleOneinner1  d-flex justify-content-center'>
             <div className='circleOneinner2  d-flex justify-content-center'>
               <div className='circleOneinner3 d-flex flex-column justify-content-center shadow'>
                 <div className='circleOneinner4 d-flex flex-column justify-content-center shadow '>
-                  <h1 className='text-dark shadow'>Event</h1>
-                  <h2 className='text-dark shadow'>Management</h2>
-                  <button className='rounded w-50 mt-2' style={{ backgroundColor: '#e6ffff' }}>start</button>
-                </div>
+                  <h1 className='text-dark shadow fs-1 fw-bold'>Event</h1>
+                  <h2 className='text-dark shadow fs-2 fw-bold'>Management</h2>
+                  <h4 className='text-dark fw-bold shadow'>System</h4>
+                  <button
+  className="rounded w-50 mt-2 glowing-button"
+  style={{ backgroundColor: '#e6ffff', border: 'none', cursor: 'pointer' }}
+>
+  Start
+</button>
+  
+                         </div>
               </div>
             </div>
           </div>
         </div>
 
-       
+
 
         <div className='circleTwo d-flex flex-column justify-content-center shadow'>
           <div className='circleinnerIn d-flex flex-column justify-content-center shadow text-dark'>
-          <i class="fa-solid fa-trash fa-3x"></i>
+            <i class="fa-solid fa-trash fa-3x"></i>
           </div>
         </div>
 
         <div className='circleThree d-flex flex-column justify-content-center shadow'>
           <div className='circleinnerIn d-flex flex-column justify-content-center shadow text-dark'>
-          <i class="fa-solid fa-calendar-plus fa-4x"></i>
+            <i class="fa-solid fa-calendar-plus fa-4x"></i>
           </div>
         </div>
 
