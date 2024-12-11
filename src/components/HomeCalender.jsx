abd4d18377168f722091c5ac5ec57c2208b425b6
NAJIYA KP <najiyabinthmajeed@gmail.com>
2024-12-11 06:34:23 -0800

    drag and drop completed

--- src/components/HomeCalender.jsx
+++ src/components/HomeCalender.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react'
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
+import { getSinglEvent } from '../services/allApi';
+
 
 
 function HomeCalender() {
@@ -8,9 +10,39 @@ function HomeCalender() {
     const [value, onChange] = useState(new Date());
 
 
-    const eventDropped=(e)=>{
+    const eventDropped=async(e)=>{
         const eventsId=e.dataTransfer.getData("eventId")
         console.log(`event dropped with ${eventsId}`)
+
+        try{
+
+
+            const {data}=await getSinglEvent(eventsId)
+            console.log(data)
+    
+            const {eventDate}=data
+            console.log(eventDate)
+    
+            const eventDates= new Date(data.eventDate)
+    
+            
+            const moths=eventDates.getMonth()// give 0-jan
+            console.log(moths)
+            
+            const days=eventDates.getDate()+1
+            console.log(days)
+
+            const years=eventDates.getFullYear()
+            console.log(years)
+
+            const eveDate=new Date(years,moths,days);
+            onChange(eveDate)
+
+
+        }catch(err){
+            console.log(err)
+        }
+       
 
     }
 
